bdbce6eb1101b456fcdfcf158b3aac5093970226
Karen Wang <kwang022020@gmail.com>
2022-09-02 11:10:37 -0700

    finished WK21 Assessment - Input and Output

--- src/main/java/com/kenzie/app/FruitHttpClient.java
+++ src/main/java/com/kenzie/app/FruitHttpClient.java
@@ -11,50 +11,100 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FruitHttpClient {
     public static String getDesiredURL(FruitCriteria fc){
         switch(fc){
             case ALL:
                 // look through the documentation to determine the correct URL
-                return "";
+                return "https://www.fruityvice.com/api/fruit/all";
             case LOW_CARB:
                 // look through the documentation to determine the correct URL
-                return "";
+                return "https://www.fruityvice.com/api/fruit/carbohydrates?min=0&max=5";
             case HIGH_CALORIE:
                 // look through the documentation to determine the correct URL
-                return "";
+                return "https://www.fruityvice.com/api/fruit/calories?min=100&max=1000";
             default:
                 return "";
         }
     }
 
     public String getFruits(String url) {
-        // TODO: create a URL object from the desired URL
-        // TODO: make the GET request and return the list of FruitDTOs
-        return "";
+
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 200) {
+                return httpResponse.body();
+            } else {
+                return String.format("GET request failed: %d status code received", statusCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
     }
 
     public static List<FruitDTO> getFruitsList(String httpResponseBody) throws JsonProcessingException {
-        return new ArrayList<FruitDTO>();
+        List<FruitDTO> fruitListObj;
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<FruitDTO>> typeRefList = new TypeReference<>(){};
+        //use ObjectMapper and readValue
+        fruitListObj = objectMapper.readValue(httpResponseBody,typeRefList);
+        return fruitListObj;
     }
     
         public static FruitDTO createFruitDTO(){
-        // TODO: create a sample FruitDTO object based on what you want to send in your PUT request
         // instantiate a new FruitDTO object and a new Nutritions DTO object
         // then fill in the properties for each item with the setter methods
-        return new FruitDTO();
+            FruitDTO fruitDTOObj = new FruitDTO();
+            fruitDTOObj.setGenus("Fragaria");
+            fruitDTOObj.setName("Blackberry");
+            fruitDTOObj.setFamily("Rosaceae");
+            fruitDTOObj.setOrder("Rosales");
+            FruitDTO.Nutritions nutritions = new FruitDTO.Nutritions();
+            nutritions.setCarbohydrates(5.5);
+            nutritions.setProtein(0);
+            nutritions.setFat(0.4);
+            nutritions.setCalories(29);
+            nutritions.setSugar(5.4);
+        return fruitDTOObj;
     }
 
     public static String fruitToJSON(FruitDTO fruit) throws JsonProcessingException {
-        // TODO: convert FruitDTO to JSON using the Jackson library
-        return "";
+        ObjectMapper objectMapper = new ObjectMapper();
+     // configure Object mapper for pretty print
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        String fruitStr = objectMapper.writeValueAsString(fruit);
+        return fruitStr;
     }
 
     public String putFruit(String fruitStr, String url) {
-        // TODO: make a PUT request with the data, and return the response
-        return "";
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(fruitStr))
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 201 || statusCode == 202) {
+                return httpResponse.body();
+            } else {
+                return String.format("PUT request failed: %d status code received", statusCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
     }
 }
